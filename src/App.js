f0b30923de9d07db3b8b69f98c8c13a8a86df948
shanmugakalap <shanmugakala.p@kaditinnovations.com>
2024-10-24 11:55:30 +0530

    app.js commited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StudentForm from './components/StudentForm';
+import StudentSearchForm from './components/StudentSearchForm';
+import StudentUpdateForm from './components/StudentUpdateForm';
+import Navigation from './components/Navigation'; // Import the Navigation component
+import HomePage from './components/homepage';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <StudentForm />
-        </div>
+        <Router>
+            <div className="App">
+                <Navigation /> {/* Add the navigation menu */}
+                <Routes>
+                    <Route path ="" element={<HomePage/>} />
+                    <Route path="/students/search" element={<StudentSearchForm />} />
+                    <Route path="/students/create" element={<StudentForm />} />
+                    <Route path="/students/update/:studentId" element={<StudentUpdateForm />} />
+                </Routes>
+            </div>
+        </Router>
     );
-}
+};
 
 export default App;
